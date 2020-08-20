1ec11c5166dfd8cf89e3cc02e7d51e22b760d294
michal.michalski <michal.michalski@talktalkplc.com>
2020-08-20 15:25:06 +0100

    New changes

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-console.log('siema');
-const name = 'Michal';
+console.log("siema");
+const name = "Michal";
 const age = 31;
 
 console.log(name);
 console.log(age);
 
 console.log(`Nazywam sie ${name} i mam ${age} lat`);
+
+function calculate(myNumber) {
+  return myNumber * 7;
+}
+
+var number = calculate(1);
+
+console.log(number);
